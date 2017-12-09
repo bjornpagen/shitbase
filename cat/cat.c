b5c87c4a75b7e26a9e1e82af9315e1a07253f839
Bjorn Pagen <bjornpagen@gmail.com>
2017-12-09 12:17:24 -0500

    added cat

---
+++ cat/cat.c
@@ -0,0 +1,37 @@
+//cat.c
+
+#include <stdio.h>
+#include <err.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main(int argc, char** argv) {
+	if(argc <= 1) {
+		// read from stdin, and print it back to stdout
+		char c;
+		while(c = fgetc(stdin)) {
+			fputc(c, stdout);
+		}
+		return 0;
+	} else {
+		// print the file at argv[1]
+		char* filename = argv[1];
+		long size;
+		char c;
+
+		FILE* rfp = fopen(filename, "rb");
+		fseek(rfp, 0, SEEK_END);
+		size = ftell(rfp);
+		fseek(rfp, 0, SEEK_SET);
+
+		for(int n = 0; n < size; n++) {
+			c = fgetc(rfp);
+			fputc(c, stdout);
+		}
+
+		fclose(rfp);
+		return 0;
+	}
+}
