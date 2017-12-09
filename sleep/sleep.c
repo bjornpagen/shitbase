ac82416a0c8b55b392ea8bf8c0f34d1077402fe4
Bjorn Pagen <bjornpagen@gmail.com>
2017-12-09 14:28:03 -0500

    add sleep

---
+++ sleep/sleep.c
@@ -0,0 +1,15 @@
+// sleep.c
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	if(argc < 2) {
+		fprintf(stderr,"%s: missing operand\n", argv[0]);
+		return 1;
+	} else {
+		sleep(atoi(argv[1]));
+	}
+	return 0;
+}
